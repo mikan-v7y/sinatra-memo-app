caadca04e7a0893e51e007eed75e558240213862
mikan-v7y <mikan.v7y@gmail.com>
2025-04-02 16:42:49 +0900

    feature:CSS対応

---
+++ app.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'erb'
+require 'json'
+require 'fileutils'
+
+helpers do
+  def h(text)
+    ERB::Util.html_escape(text)
+  end
+end
+
+class SomeClass
+  include ERB::Util
+end
+
+SAVE_FILE = 'save_file.json'
+
+def load_memos
+  if File.empty?(SAVE_FILE)
+    []
+  else
+    JSON.parse(File.read(SAVE_FILE))
+  end
+end
+
+def save_memos(memos)
+  json_memos = JSON.generate(memos)
+  File.write(SAVE_FILE, json_memos)
+end
+
+get '/memos' do
+  @memos = load_memos
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new_memo
+end
+
+post '/memos' do
+  title = params[:title]
+  content = params[:content]
+  memos = load_memos
+  new_memo = { 'id' => memos.size + 1, 'title' => title, 'content' => content }
+  memos << new_memo
+  save_memos(memos)
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  @memo = load_memos.find { |memo| memo['id'] == params[:id].to_i }
+  erb :show
+end
+
+get '/memos/:id/editing' do
+  @memo = load_memos.find { |memo| memo['id'] == params[:id].to_i }
+  erb :edit
+end
+
+patch '/memos/:id/editing' do
+  memos = load_memos
+  memo = memos.find { |memo| memo['id'] == params[:id].to_i }
+  memo['title'] = params[:title]
+  memo['content'] = params[:content]
+  save_memos(memos)
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id/deletion' do
+  memos = load_memos
+  memos.reject! { |memo| memo['id'] == params[:id].to_i }
+  save_memos(memos)
+  redirect '/memos'
+end
