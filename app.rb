29aa86f3aa7fe42bc205880b6ef9f598c31f1f59
mikan-v7y <mikan.v7y@gmail.com>
2025-05-06 17:48:48 +0900

    refactor: rubocopの指摘箇所を修正

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ end
 MEMO_RECORDS_FILE = 'memos.json'
 
 get '/memos' do
-  @memos = DB.exec("SELECT * FROM memos ORDER BY id").map do |row|
+  @memos = DB.exec('SELECT * FROM memos ORDER BY id').map do |row|
     { id: row['id'].to_i, title: row['title'], content: row['content'] }
   end
   erb :index
@@ -30,7 +30,7 @@ end
 
 post '/memos' do
   DB.exec_params(
-    "INSERT INTO memos (title, content) VALUES ($1, $2)",
+    'INSERT INTO memos (title, content) VALUES ($1, $2)',
     [params[:title], params[:content]]
   )
   redirect '/memos'
@@ -53,14 +53,14 @@ end
 
 patch '/memos/:id' do
   DB.exec_params(
-    "UPDATE memos SET title = $1, content = $2 WHERE id = $3",
+    'UPDATE memos SET title = $1, content = $2 WHERE id = $3',
     [params[:title], params[:content], params[:id]]
   )
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  DB.exec_params("DELETE FROM memos WHERE id = $1", [params[:id]])
+  DB.exec_params('DELETE FROM memos WHERE id = $1', [params[:id]])
   redirect '/memos'
 end
 
@@ -70,8 +70,8 @@ not_found do
 end
 
 def find_memo(id)
-  result = DB.exec_params("SELECT * FROM memos WHERE id = $1 LIMIT 1", [id])
-  return nil if result.ntuples == 0
+  result = DB.exec_params('SELECT * FROM memos WHERE id = $1 LIMIT 1', [id])
+  return nil if result.ntuples.zero?
 
   row = result[0]
   { id: row['id'].to_i, title: row['title'], content: row['content'] }
